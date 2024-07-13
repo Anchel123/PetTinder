ec7ec67e6284bfc34524f2c79a24674e65cb6709
Anchel135 <yakoveidelman1@gmail.com>
2024-07-13 18:00:14 +0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import LandingPage from "./components/LandingPage";
 
 export default function Home() {
   return (
-    <div className="h-1 grow">
-      <main className="w-full h-full">
-        <LandingPage/>
-      </main>
+    <div className="Page">
+      <LandingPage />
     </div>
   );
 }
