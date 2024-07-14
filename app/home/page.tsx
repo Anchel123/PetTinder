369674aec2474c6dea52a9bd812fed14d66cd805
Anchel135 <yakoveidelman1@gmail.com>
2024-07-14 15:10:55 +0300

    commit

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import PetRoller from "./PetRoller";
+
+export default function Page() {
+    return (
+        <div className="Page h-1 grow">
+            <PetRoller/>
+        </div>
+    )
+}
